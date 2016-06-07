b9fbb6df0e9a15cea51377800a3b034844d92788
Haozhe Chen <haozhechen@hotmail.com>
2016-06-07 14:13:24 +0100

    Initial project commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author haozhechen
+ */
+public class Main {
+    
+    public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
+    
+    public static void main(String[] args){
+        System.out.println("Success");
+        
+        String testVariable = "Parsing";
+        String testLocation = "Phase Three";
+        
+       LOGGER.info("Error: {} Location: {}", testVariable, testLocation);
+      
+    }
+    
+}
