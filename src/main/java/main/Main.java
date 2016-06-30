ace1a6d619be7374e56da12177c9e2654e27ad96
Haozhe Chen <haozhechen@hotmail.com>
2016-06-30 01:26:23 +0100

    build.gradle -exclude Jena's slf4j part
    distance.java
    -some ideas about the geof:distance implementation

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -14,6 +14,21 @@ import org.slf4j.LoggerFactory;
  */
 public class Main {
     
+    /* -------------------------------------------------- 
+    GeoSPARQL Name Spaces
+    ogc: http://www.opengis.net/
+    geo: http://www.opengis.net/ont/geosparql#
+    geof: http://www.opengis.net/def/function/geosparql/
+    geor: http://www.opengis.net/def/rule/geosparql/
+    sf: http://www.opengis.net/ont/sf#
+    gml: http://www.opengis.net/ont/gml#
+    my: http://example.org/ApplicationSchema#
+    xsd: http://www.w3.org/2001/XMLSchema#
+    rdf: http://www.w3.org/1999/02/22-rdf-syntax-ns#
+    rdfs: http://www.w3.org/2000/01/rdf-schema#
+    owl: http://www.w3.org/2002/07/owl#
+    -------------------------------------------------- */ 
+    
     public static final Logger LOGGER = LoggerFactory.getLogger(Main.class);
     
     public static void main(String[] args){
@@ -28,4 +43,13 @@ public class Main {
       
     }
     
+    public String getFullName(String firstName, String secondName){
+        
+        return firstName + " " + secondName;
+    }
+    
+    public String getUserName(String firstName, String secondName){
+        
+        return firstName + secondName;
+    }
 }
