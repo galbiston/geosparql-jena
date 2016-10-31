aea7b135a28138a56ecabd0bbc92099cd2d6d482
Greg Albiston <greg_albiston@hotmail.com>
2016-10-31 16:01:44 +0000

    geof.nontopological.filterfunction classes renamed to remove FilterFunc.
    Former-commit-id: 85b217308f00b99a4633ace971240e714a564445

---
+++ src/main/java/geof/nontopological/filterfunction/Buffer.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geof.nontopological.filterfunction;
+
+import datatype.DistanceUnitsEnum;
+import datatype.GeometryWrapper;
+import datatype.UomConverter;
+import org.apache.jena.datatypes.DatatypeFormatException;
+import org.apache.jena.graph.Node;
+import org.apache.jena.sparql.expr.NodeValue;
+import org.apache.jena.sparql.function.FunctionBase3;
+
+/**
+ *
+ * Return type ogc:geomLiteral ogc:geomLiteral can be a string value like
+ * "<gml:Point ...>...</gml:Point>"
+ *
+ * @author haozhechen
+ */
+public class Buffer extends FunctionBase3 {
+
+    @Override
+    public NodeValue exec(NodeValue v1, NodeValue v2, NodeValue v3) {
+
+        try {
+            GeometryWrapper geometry = GeometryWrapper.extract(v1);
+
+            //Transfer the parameters as Nodes
+            Node node2 = v2.asNode();
+            double radius = Double.parseDouble(node2.getLiteralLexicalForm());
+
+            //Obtain the target distance units
+            DistanceUnitsEnum targetDistanceUnits = UomConverter.extract(v3);
+
+            GeometryWrapper buffer = geometry.buffer(radius, targetDistanceUnits);
+
+            return buffer.getResultNode();
+        } catch (DatatypeFormatException dfx) {
+            return NodeValue.nvEmptyString;
+        }
+
+    }
+}
---
+++ src/main/java/geof/nontopological/filterfunction/ConvexHull.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geof.nontopological.filterfunction;
+
+import datatype.GeometryWrapper;
+import org.apache.jena.datatypes.DatatypeFormatException;
+import org.apache.jena.sparql.expr.NodeValue;
+import org.apache.jena.sparql.function.FunctionBase1;
+
+/**
+ *
+ * @author haozhechen
+ */
+public class ConvexHull extends FunctionBase1 {
+
+    @Override
+    public NodeValue exec(NodeValue v) {
+
+        try {
+            GeometryWrapper geometry = GeometryWrapper.extract(v);
+
+            GeometryWrapper convexHull = geometry.convexHull();
+            return convexHull.getResultNode();
+
+        } catch (DatatypeFormatException dfx) {
+            return NodeValue.nvEmptyString;
+        }
+
+    }
+
+}
