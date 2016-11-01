31132a15dd31d1a12b35c14d28da63a4f0cc1623
Greg Albiston <greg_albiston@hotmail.com>
2016-11-01 07:45:58 +0000

    Egenhofer Meet functions multiple relates replaced by use of Intersection Matrix.
    Former-commit-id: 9cb5235ca6a18b27aac13e8d3c4b953814680610

--- src/main/java/geof/topological/egenhofer/filterfunction/Meet.java
+++ src/main/java/geof/topological/egenhofer/filterfunction/Meet.java
@@ -5,8 +5,9 @@
  */
 package geof.topological.egenhofer.filterfunction;
 
-import implementation.GeometryWrapper;
+import com.vividsolutions.jts.geom.IntersectionMatrix;
 import geof.topological.GenericFilterFunction;
+import implementation.GeometryWrapper;
 import org.opengis.geometry.MismatchedDimensionException;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.operation.TransformException;
@@ -21,7 +22,8 @@ public class Meet extends GenericFilterFunction {
     @Override
     protected boolean relate(GeometryWrapper sourceGeometry, GeometryWrapper targetGeometry) throws FactoryException, MismatchedDimensionException, TransformException {
 
-        return sourceGeometry.relate(targetGeometry, "FT*******") || sourceGeometry.relate(targetGeometry, "F**T*****") || sourceGeometry.relate(targetGeometry, "F***T****");
+        IntersectionMatrix matrix = sourceGeometry.relate(targetGeometry);
+        return matrix.matches("FT*******") || matrix.matches("F**T*****") || matrix.matches("F***T****");
 
         //TODO Reduce DE-9IM to just ensure the interiors don't overlap
         //See http://docs.geotools.org/stable/userguide/library/jts/dim9.html
