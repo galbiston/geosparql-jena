989c299a61901349090028ffc5184964186edb76
Greg Albiston <greg_albiston@hotmail.com>
2017-07-07 13:38:55 +0100

    GeoSPARQL:GeometryClassTest - removed unnecessary test data - corrected test data - ordered results for consistency - removed unnecessary test setup Former-commit-id: 374957f7795d8ab09640aaf53b2d4d657e5b4d07

--- src/test/java/conformanceTest/geometryextension/GeometryClassTest.java
+++ src/test/java/conformanceTest/geometryextension/GeometryClassTest.java
@@ -8,6 +8,7 @@ package conformanceTest.geometryextension;
 import static conformanceTest.ConformanceTestSuite.*;
 import static implementation.functionregistry.RegistryLoader.load;
 import java.util.ArrayList;
+import java.util.List;
 import org.junit.After;
 import org.junit.AfterClass;
 import static org.junit.Assert.assertEquals;
@@ -21,14 +22,13 @@ import org.junit.Test;
  *
  * A.3.1.1 /conf/geometry-extension/geometry-class
  *
- * Requirement: /req/geometry-extension/geometry-class
- * Implementations shall allow the RDFS class geo:Geometry to be used in
- * SPARQL graph patterns.
+ * Requirement: /req/geometry-extension/geometry-class Implementations shall
+ * allow the RDFS class geo:Geometry to be used in SPARQL graph patterns.
  *
  * a.) Test purpose: check conformance with this requirement
  *
- * b.) Test method: verify that queries involving geo:Geometry return
- * the correct result on a test dataset.
+ * b.) Test method: verify that queries involving geo:Geometry return the
+ * correct result on a test dataset.
  *
  * c.) Reference: Clause 8.2.1 Req 7 d.) Test Type: Capabilities
  */
@@ -47,40 +47,35 @@ public class GeometryClassTest {
     public static void tearDownClass() {
     }
 
-    private ArrayList expectedList;
-    private ArrayList actualList;
-
     @Before
     public void setUp() {
-        this.expectedList = new ArrayList<>();
-        this.actualList = new ArrayList<>();
+
     }
 
     @After
     public void tearDown() {
-        this.actualList.clear();
-        this.expectedList.clear();
+
     }
 
     @Test
     public void positiveTest() {
 
-        this.expectedList.add("http://example.org/ApplicationSchema#BExactGeom");
-        this.expectedList.add("http://example.org/ApplicationSchema#CExactGeom");
-        this.expectedList.add("http://example.org/ApplicationSchema#AExactGeom");
-        this.expectedList.add("http://example.org/ApplicationSchema#FPointGeom");
-        this.expectedList.add("http://example.org/ApplicationSchema#EExactGeom");
-        this.expectedList.add("http://example.org/ApplicationSchema#CPointGeom");
-        this.expectedList.add("http://example.org/ApplicationSchema#DPointGeom");
-        this.expectedList.add("http://example.org/ApplicationSchema#DExactGeom");
-        this.expectedList.add("http://example.org/ApplicationSchema#FExactGeom");
-        this.expectedList.add("http://example.org/ApplicationSchema#EPointGeom");
+        List<String> expectedList = new ArrayList<>();
+
+        expectedList.add("http://example.org/ApplicationSchema#AExactGeom");
+        expectedList.add("http://example.org/ApplicationSchema#BExactGeom");
+        expectedList.add("http://example.org/ApplicationSchema#CExactGeom");
+        expectedList.add("http://example.org/ApplicationSchema#DExactGeom");
+        expectedList.add("http://example.org/ApplicationSchema#EExactGeom");
+        expectedList.add("http://example.org/ApplicationSchema#FExactGeom");
+        expectedList.add("http://example.org/ApplicationSchema#GExactGeom");
 
         String Q1 = "SELECT ?geometry WHERE{"
                 + " ?geometry rdf:type geo:Geometry ."
-                + "}";
-        this.actualList = resourceQuery(Q1, INF_WKT_MODEL);
-        assertEquals("failure - result arrays list not same", this.expectedList, this.actualList);
+                + "}ORDER BY ?geometry";
+        List<String> actualList = resourceQuery(Q1, INF_WKT_MODEL);
+
+        assertEquals("failure - result arrays list not same", expectedList, actualList);
     }
 
 }
