7288b0446e10f05dc0a9ef59adf28e926b04df71
Greg Albiston <greg_albiston@hotmail.com>
2017-08-02 16:06:40 +0100

    GeoSPARQL: - filter functions created to re-use existing expression function code but only function loading to use FunctionRegistry approach described in Apache documentation. Former-commit-id: c48d70b93055b33d755811f2941e9fea1ee7728a

---
+++ src/main/java/geof/topological/rcc8/FF/rccNonTangentialProperPartFF.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geof.topological.rcc8.FF;
+
+import geof.topological.rcc8.EF.rccNonTangentialProperPartEF;
+import org.apache.jena.sparql.expr.Expr;
+import org.apache.jena.sparql.expr.NodeValue;
+import org.apache.jena.sparql.function.FunctionBase2;
+
+/**
+ *
+ * @author Greg Albiston
+ */
+public class rccNonTangentialProperPartFF extends FunctionBase2 {
+
+    @Override
+    public NodeValue exec(NodeValue v1, NodeValue v2) {
+
+        Expr e1 = v1.getExpr();
+        Expr e2 = v2.getExpr();
+
+        rccNonTangentialProperPartEF func = new rccNonTangentialProperPartEF(e1, e2);
+
+        return func.eval(v1, v2);
+    }
+
+}
---
+++ src/main/java/geof/topological/simplefeatures/FF/sfWithinFF.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package geof.topological.simplefeatures.FF;
+
+import geof.topological.simplefeatures.EF.sfWithinEF;
+import org.apache.jena.sparql.expr.Expr;
+import org.apache.jena.sparql.expr.NodeValue;
+import org.apache.jena.sparql.function.FunctionBase2;
+
+/**
+ *
+ * @author Greg Albiston
+ */
+public class sfWithinFF extends FunctionBase2 {
+
+    @Override
+    public NodeValue exec(NodeValue v1, NodeValue v2) {
+
+        Expr e1 = v1.getExpr();
+        Expr e2 = v2.getExpr();
+
+        sfWithinEF func = new sfWithinEF(e1, e2);
+
+        return func.eval(v1, v2);
+    }
+
+}
