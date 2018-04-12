82b96769d5562ec4834abb6cab2245ed9c90f6ee
Greg Albiston <greg_albiston@hotmail.com>
2018-04-12 13:20:34 +0100

    ConvertCRS: Fixed file conversion. Added folder conversion method. Re-organised stages into private methods. Former-commit-id: 95197895aa92330a52480e73d8d9314ac9b49570

--- src/main/java/implementation/ConvertCRS.java
+++ src/main/java/implementation/ConvertCRS.java
@@ -11,14 +11,13 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.lang.invoke.MethodHandles;
 import java.util.Iterator;
 import org.apache.jena.datatypes.RDFDatatype;
 import org.apache.jena.rdf.model.Literal;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.rdf.model.RDFNode;
 import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.rdf.model.Statement;
 import org.apache.jena.riot.Lang;
@@ -36,6 +35,8 @@ import org.slf4j.LoggerFactory;
 public class ConvertCRS {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    private static final WKTDatatype WKT_DATATYPE = WKTDatatype.THE_WKT_DATATYPE;
+    private static final GMLDatatype GML_DATATYPE = GMLDatatype.THE_GML_DATATYPE;
 
     /**
      * Converts all geometry literals (WKT or GML) from current CRS to the
@@ -45,18 +46,17 @@ public class ConvertCRS {
      * @param inputLang
      * @param outputFile
      * @param outputLang
-     * @param srsURI
+     * @param outputSrsURI
      */
-    public static final void convertFile(File inputFile, Lang inputLang, File outputFile, Lang outputLang, String srsURI) {
+    public static final void convertFile(File inputFile, Lang inputLang, File outputFile, Lang outputLang, String outputSrsURI) {
 
-        WKTDatatype wktDatatype = WKTDatatype.THE_WKT_DATATYPE;
-        GMLDatatype gmlDatatype = GMLDatatype.THE_GML_DATATYPE;
+        LOGGER.info("Converting File: {} to {} in srs URI: {} - Started", inputFile.getAbsolutePath(), outputFile.getAbsolutePath(), outputSrsURI);
 
         Model model = ModelFactory.createDefaultModel();
-        try (InputStream inputStream = new FileInputStream(inputFile)) {
+        try (FileInputStream inputStream = new FileInputStream(inputFile)) {
             RDFDataMgr.read(model, inputStream, inputLang);
         } catch (IOException ex) {
-            LOGGER.error("Input File IO Exception: {} - {}", inputFile, ex.getMessage());
+            LOGGER.error("Input File IO Exception: {} - {}", inputFile.getAbsolutePath(), ex.getMessage());
         }
 
         Model outputModel = ModelFactory.createDefaultModel();
@@ -64,22 +64,9 @@ public class ConvertCRS {
         Iterator<Statement> statementIt = model.listStatements();
         while (statementIt.hasNext()) {
             Statement statement = statementIt.next();
-            Literal literal = statement.getLiteral();
-
-            RDFDatatype datatype = literal.getDatatype();
-
-            //Check whether a supported geometry literal.
-            if (datatype.equals(wktDatatype) | datatype.equals(gmlDatatype)) {
-                GeometryWrapper originalGeom = GeometryWrapper.extract(literal);
-                GeometryWrapper convertedGeom;
-                try {
-                    convertedGeom = originalGeom.convertCRS(srsURI);
-                } catch (FactoryException | MismatchedDimensionException | TransformException ex) {
-                    LOGGER.error("CRS Conversion Exception: {} - Literal: {}, Output SRS URI: {}. Reusing original literal for output.", ex.getMessage(), literal, srsURI);
-                    convertedGeom = originalGeom;
-                }
-                Statement outputStatement = ResourceFactory.createStatement(statement.getSubject(), statement.getPredicate(), convertedGeom.asLiteral());
-                outputModel.add(outputStatement);
+            RDFNode object = statement.getObject();
+            if (object.isLiteral()) {
+                handleLiteral(statement, outputModel, outputSrsURI);
             } else {
                 //Not a statement of interest so store for output.
                 outputModel.add(statement);
@@ -87,14 +74,82 @@ public class ConvertCRS {
         }
 
         //Write the output.
+        writeOutputModel(outputModel, outputFile, outputLang, inputFile);
+
+        LOGGER.info("Converting File: {} to {} in srs URI: {} - Completed", inputFile.getAbsolutePath(), outputFile.getAbsolutePath(), outputSrsURI);
+    }
+
+    private static void handleLiteral(Statement statement, Model outputModel, String outputSrsURI) {
+        Literal literal = statement.getLiteral();
+
+        RDFDatatype datatype = literal.getDatatype();
+
+        //Check whether a supported geometry literal.
+        if (datatype.equals(WKT_DATATYPE) | datatype.equals(GML_DATATYPE)) {
+            GeometryWrapper originalGeom = GeometryWrapper.extract(literal);
+            GeometryWrapper convertedGeom;
+            try {
+                convertedGeom = originalGeom.convertCRS(outputSrsURI);
+            } catch (FactoryException | MismatchedDimensionException | TransformException ex) {
+                LOGGER.error("CRS Conversion Exception: {} - Literal: {}, Output SRS URI: {}. Reusing original literal for output.", ex.getMessage(), literal, outputSrsURI);
+                convertedGeom = originalGeom;
+            }
+            Statement outputStatement = ResourceFactory.createStatement(statement.getSubject(), statement.getPredicate(), convertedGeom.asLiteral());
+            outputModel.add(outputStatement);
+        } else {
+            //Not a statement of interest so store for output.
+            outputModel.add(statement);
+        }
+    }
+
+    private static void writeOutputModel(Model outputModel, File outputFile, Lang outputLang, File inputFile) {
         if (!outputModel.isEmpty()) {
-            try (OutputStream outputStream = new FileOutputStream(outputFile)) {
+            try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
                 RDFDataMgr.write(outputStream, outputModel, outputLang);
             } catch (IOException ex) {
-                LOGGER.error("Output File IO Exception: {} - {}", outputFile, ex.getMessage());
+                LOGGER.error("Output File IO Exception: {} - {}", outputFile.getAbsolutePath(), ex.getMessage());
             }
+        } else {
+            LOGGER.warn("Output Model is empty for {}: Did not create: {}", inputFile.getAbsolutePath(), outputFile.getAbsolutePath());
         }
+    }
 
+    /**
+     * Only RDF files should be in the input folder and must all be the same RDF
+     * language.
+     *
+     * @param inputFolder
+     * @param inputLang
+     * @param outputFolder
+     * @param outputLang
+     * @param outputSrsURI
+     */
+    public static final void convertFolder(File inputFolder, Lang inputLang, File outputFolder, Lang outputLang, String outputSrsURI) {
+
+        LOGGER.info("Converting Folder {} to {} in srs URI: {} - Started", inputFolder.getAbsolutePath(), outputFolder.getAbsolutePath(), outputSrsURI);
+        if (inputFolder.exists()) {
+            File[] inputFiles = inputFolder.listFiles();
+
+            if (inputFiles.length > 0) {
+                outputFolder.mkdir();
+
+                for (File inputFile : inputFiles) {
+                    File outputFile = new File(outputFolder, inputFile.getName());
+                    try {
+                        convertFile(inputFile, inputLang, outputFile, outputLang, outputSrsURI);
+                    } catch (Exception ex) {
+                        LOGGER.error("{} for input {}. The output file {} may not be created.", ex.getMessage(), inputFile.getAbsolutePath(), outputFile.getAbsolutePath());
+                    }
+                }
+
+            } else {
+                LOGGER.warn("{} is empty. {} is not created.", inputFolder.getAbsolutePath(), outputFolder.getAbsolutePath());
+            }
+
+        } else {
+            LOGGER.warn("{} does not exist. {} is not created.", inputFolder.getAbsolutePath(), outputFolder.getAbsolutePath());
+        }
+        LOGGER.info("Converting Folder {} to {} in srs URI: {} - Completed", inputFolder.getAbsolutePath(), outputFolder.getAbsolutePath(), outputSrsURI);
     }
 
 }
