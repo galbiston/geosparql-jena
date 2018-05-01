3d93d3f043167338002f4044e1ba226fc7836e8a
Greg Albiston <greg_albiston@hotmail.com>
2018-05-01 15:35:32 +0100

    GeoSPARQLPredicates to add predicates to datasets if not asserted.
    Former-commit-id: ae8167f1daa7514291eac375cd2edf2e02657b57

---
+++ src/main/java/implementation/data_conversion/GeoSPARQLPredicates.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package implementation.data_conversion;
+
+import implementation.support.QueryLoader;
+import static implementation.data_conversion.ConvertCRS.writeOutputModel;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ModelFactory;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.update.UpdateAction;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ *
+ */
+public class GeoSPARQLPredicates {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    /**
+     * Infer and add GeoSPARQL predicates to the model. geo:hasDefaultGeometry,
+     * geo:hasGeometry, geo:asWKT, geo:asGML
+     *
+     * @param model
+     */
+    public static final void apply(Model model) {
+
+        //Assumption 1: that each geo:Feature has one geo:hasGeometry relationship so that geo:hasDefaultGeometry only exists once for each.
+        //Assumption 2: that Feature predicate1 Thing and Thing predicate2 (wktLiteral||gmlLiteral) makes Thing a geo:Geometry.
+        //Find the gmlLiteral and wktLiteral datatypes in the data. Then find
+        try {
+            String insertString = QueryLoader.readResource("sparql_query/InsertDatatypeSubProperty.spl");
+            UpdateAction.parseExecute(insertString, model);
+        } catch (Exception ex) {
+            LOGGER.error("Inserting GeoSPARQL predicates error: {}", ex.getMessage());
+        }
+    }
+
+    public static final void applyFile(File inputFile, Lang inputLang, File outputFile, Lang outputLang) {
+
+        LOGGER.info("Applying Predicates from File: {} to {} - Started", inputFile.getAbsolutePath(), outputFile.getAbsolutePath());
+
+        Model model = ModelFactory.createDefaultModel();
+        try (FileInputStream inputStream = new FileInputStream(inputFile)) {
+            RDFDataMgr.read(model, inputStream, inputLang);
+            //Write the output.
+            writeOutputModel(model, outputFile, outputLang, inputFile);
+        } catch (IOException ex) {
+            LOGGER.error("Input File IO Exception: {} - {}", inputFile.getAbsolutePath(), ex.getMessage());
+        }
+
+        LOGGER.info("Applying Predicates from File: {} to {} - Completed", inputFile.getAbsolutePath(), outputFile.getAbsolutePath());
+
+    }
+
+    /**
+     * Only RDF files should be in the input folder and must all be the same RDF
+     * language.
+     *
+     * @param inputFolder
+     * @param inputLang
+     * @param outputFolder
+     * @param outputLang
+     */
+    public static final void convertFolder(File inputFolder, Lang inputLang, File outputFolder, Lang outputLang) {
+
+        LOGGER.info("Applying Predicates from Folder {} to {} - Started", inputFolder.getAbsolutePath(), outputFolder.getAbsolutePath());
+        if (inputFolder.exists()) {
+            File[] inputFiles = inputFolder.listFiles();
+
+            if (inputFiles.length > 0) {
+                outputFolder.mkdir();
+
+                for (File inputFile : inputFiles) {
+                    File outputFile = new File(outputFolder, inputFile.getName());
+                    try {
+                        applyFile(inputFile, inputLang, outputFile, outputLang);
+                    } catch (Exception ex) {
+                        LOGGER.error("{} for input {}. The output file {} may not be created.", ex.getMessage(), inputFile.getAbsolutePath(), outputFile.getAbsolutePath());
+                    }
+                }
+
+            } else {
+                LOGGER.warn("{} is empty. {} is not created.", inputFolder.getAbsolutePath(), outputFolder.getAbsolutePath());
+            }
+
+        } else {
+            LOGGER.warn("{} does not exist. {} is not created.", inputFolder.getAbsolutePath(), outputFolder.getAbsolutePath());
+        }
+        LOGGER.info("Applying Predicates from Folder {} to {} - Completed", inputFolder.getAbsolutePath(), outputFolder.getAbsolutePath());
+    }
+
+}
