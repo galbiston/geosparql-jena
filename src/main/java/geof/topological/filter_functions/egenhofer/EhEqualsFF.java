7db443448e4d6218afaefc67f886a879ed4836ba
Greg Albiston <greg_albiston@hotmail.com>
2018-05-15 10:31:29 +0100

    EhEqualsFF, RCCEqualsFF, SfEqualsFF: now do string comparison with other checks to see if identical literal for quicker result. Otherwise carry out normal unpacking and check.
    Former-commit-id: 02b0ad6646df5afda15c7264d5d6ee764fe422c2

--- src/main/java/geof/topological/filter_functions/egenhofer/EhEqualsFF.java
+++ src/main/java/geof/topological/filter_functions/egenhofer/EhEqualsFF.java
@@ -7,7 +7,11 @@ package geof.topological.filter_functions.egenhofer;
 
 import geof.topological.GenericFilterFunction;
 import implementation.GeometryWrapper;
+import implementation.datatype.GMLDatatype;
+import implementation.datatype.WKTDatatype;
 import implementation.intersection_patterns.EgenhoferIntersectionPattern;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.sparql.expr.NodeValue;
 import org.opengis.geometry.MismatchedDimensionException;
 import org.opengis.referencing.FactoryException;
 import org.opengis.referencing.operation.TransformException;
@@ -17,6 +21,38 @@ import org.opengis.referencing.operation.TransformException;
  *
  */
 public class EhEqualsFF extends GenericFilterFunction {
+
+    @Override
+    public NodeValue exec(NodeValue v1, NodeValue v2) {
+
+        if (v1.isLiteral() && v2.isLiteral()) {
+            if (v1.asString().equals(v2.asString())) {
+                String datatypeURI = v1.getDatatypeURI();
+                if (datatypeURI.equals(WKTDatatype.THE_TYPE_URI) || datatypeURI.equals(GMLDatatype.THE_TYPE_URI)) {
+                    return NodeValue.TRUE;
+                } else {
+                    return NodeValue.FALSE;
+                }
+            } else {
+                return super.exec(v1, v2);
+            }
+        }
+        return NodeValue.FALSE;
+    }
+
+    @Override
+    public Boolean exec(Literal v1, Literal v2) {
+        if (v1.getLexicalForm().equals(v2.getLexicalForm())) {
+            String datatypeURI = v1.getDatatypeURI();
+            if (datatypeURI.equals(WKTDatatype.THE_TYPE_URI) || datatypeURI.equals(GMLDatatype.THE_TYPE_URI)) {
+                return Boolean.TRUE;
+            } else {
+                return Boolean.FALSE;
+            }
+        } else {
+            return super.exec(v1, v2);
+        }
+    }
 
     @Override
     protected boolean relate(GeometryWrapper sourceGeometry, GeometryWrapper targetGeometry) throws FactoryException, MismatchedDimensionException, TransformException {
