bb8f24f242bf6e9f69f502f0c43e26eca843c592
Greg Albiston <greg_albiston@hotmail.com>
2018-05-23 16:53:36 +0100

    RDFConversion project skeleton added for CSVConversion methods.
    Former-commit-id: 27e1ac298958a729e9f6757b6864268143a37bb9

---
+++ src/main/java/implementation/data_conversion/CSVConversion.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package implementation.data_conversion;
+
+import java.lang.invoke.MethodHandles;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author Gerg
+ */
+public class CSVConversion {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    public static final void registerDatatypes() {
+        /*
+        DatatypeController.addPrefixDatatype("wkt", WKTDatatype.INSTANCE);
+        DatatypeController.addPrefixDatatype("gml", GMLDatatype.INSTANCE);
+         */
+    }
+
+}
