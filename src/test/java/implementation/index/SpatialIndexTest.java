9e2baf774298c744731bed5dba5351a3ec138ac5
Greg Albiston <greg_albiston@hotmail.com>
2018-08-28 09:14:01 +0100

    SpatialIndexTest: fixed test by activating SpatialIndex for test. Former-commit-id: d05f9b4afb431221635fde52bb1058272cd47524

--- src/test/java/implementation/index/SpatialIndexTest.java
+++ src/test/java/implementation/index/SpatialIndexTest.java
@@ -34,6 +34,7 @@ public class SpatialIndexTest {
     @BeforeClass
     public static void setUpClass() {
         GeoSPARQLSupport.loadFunctionsMemoryIndex();
+        SpatialIndex.setActive(true);
         String datatypeURI = WKTDatatype.URI;
 
         try {
@@ -47,6 +48,7 @@ public class SpatialIndexTest {
 
     @AfterClass
     public static void tearDownClass() {
+        SpatialIndex.setActive(false);
     }
 
     @Before
