a3d4f195aaf28081195afa7182b20126d8baa5a9
Greg Albiston <4662258+galbiston@users.noreply.github.com>
2019-01-11 19:50:13 +0000

    ConvertLatLonBoxFF: corrected WKT Polygon format tests. Query testing.

--- src/test/java/io/github/galbiston/geosparql_jena/spatial/filter_functions/ConvertLatLonBoxFFTest.java
+++ src/test/java/io/github/galbiston/geosparql_jena/spatial/filter_functions/ConvertLatLonBoxFFTest.java
@@ -17,6 +17,17 @@ package io.github.galbiston.geosparql_jena.spatial.filter_functions;
 
 import io.github.galbiston.geosparql_jena.configuration.GeoSPARQLConfig;
 import io.github.galbiston.geosparql_jena.implementation.datatype.WKTDatatype;
+import io.github.galbiston.geosparql_jena.spatial.SpatialIndexTestData;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ResultSet;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.ResourceFactory;
 import org.apache.jena.sparql.expr.ExprEvalException;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.junit.After;
@@ -59,11 +70,11 @@ public class ConvertLatLonBoxFFTest {
     public void testExec() {
         System.out.println("exec");
         NodeValue v1 = NodeValue.makeFloat(0.0f);
-        NodeValue v2 = NodeValue.makeFloat(0.0f);
+        NodeValue v2 = NodeValue.makeFloat(1.0f);
         NodeValue v3 = NodeValue.makeFloat(10.0f);
-        NodeValue v4 = NodeValue.makeFloat(10.0f);
+        NodeValue v4 = NodeValue.makeFloat(11.0f);
         ConvertLatLonBoxFF instance = new ConvertLatLonBoxFF();
-        NodeValue expResult = NodeValue.makeNode("<http://www.opengis.net/def/crs/EPSG/0/4326> POLYGON(0.0 0.0, 0.0 10.0, 10.0 10.0, 10.0 0.0, 0.0 0.0)", WKTDatatype.INSTANCE);
+        NodeValue expResult = NodeValue.makeNode("<http://www.opengis.net/def/crs/EPSG/0/4326> POLYGON((0.0 1.0, 10.0 1.0, 10.0 11.0, 0.0 11.0, 0.0 1.0))", WKTDatatype.INSTANCE);
         NodeValue result = instance.exec(v1, v2, v3, v4);
 
         //System.out.println("Exp: " + expResult);
@@ -78,11 +89,11 @@ public class ConvertLatLonBoxFFTest {
     public void testExec_pos0_fail() {
         System.out.println("exec_pos0_fail");
         NodeValue v1 = NodeValue.makeString("0.0");
-        NodeValue v2 = NodeValue.makeFloat(0.0f);
+        NodeValue v2 = NodeValue.makeFloat(1.0f);
         NodeValue v3 = NodeValue.makeFloat(10.0f);
-        NodeValue v4 = NodeValue.makeFloat(10.0f);
+        NodeValue v4 = NodeValue.makeFloat(11.0f);
         ConvertLatLonBoxFF instance = new ConvertLatLonBoxFF();
-        NodeValue expResult = NodeValue.makeNode("<http://www.opengis.net/def/crs/EPSG/0/4326> POLYGON(0.0 0.0, 0.0 10.0, 10.0 10.0, 10.0 0.0, 0.0 0.0)", WKTDatatype.INSTANCE);
+        NodeValue expResult = NodeValue.makeNode("<http://www.opengis.net/def/crs/EPSG/0/4326> POLYGON((0.0 1.0, 10.0 1.0, 10.0 11.0, 0.0 11.0, 0.0 1.0))", WKTDatatype.INSTANCE);
         NodeValue result = instance.exec(v1, v2, v3, v4);
 
         //System.out.println("Exp: " + expResult);
@@ -97,11 +108,11 @@ public class ConvertLatLonBoxFFTest {
     public void testExec_pos1_fail() {
         System.out.println("exec_pos1_fail");
         NodeValue v1 = NodeValue.makeFloat(0.0f);
-        NodeValue v2 = NodeValue.makeString("0.0");
+        NodeValue v2 = NodeValue.makeString("1.0");
         NodeValue v3 = NodeValue.makeFloat(10.0f);
-        NodeValue v4 = NodeValue.makeFloat(10.0f);
+        NodeValue v4 = NodeValue.makeFloat(11.0f);
         ConvertLatLonBoxFF instance = new ConvertLatLonBoxFF();
-        NodeValue expResult = NodeValue.makeNode("<http://www.opengis.net/def/crs/EPSG/0/4326> POLYGON(0.0 0.0, 0.0 10.0, 10.0 10.0, 10.0 0.0, 0.0 0.0)", WKTDatatype.INSTANCE);
+        NodeValue expResult = NodeValue.makeNode("<http://www.opengis.net/def/crs/EPSG/0/4326> POLYGON((0.0 1.0, 10.0 1.0, 10.0 11.0, 0.0 11.0, 0.0 1.0))", WKTDatatype.INSTANCE);
         NodeValue result = instance.exec(v1, v2, v3, v4);
 
         //System.out.println("Exp: " + expResult);
@@ -116,11 +127,11 @@ public class ConvertLatLonBoxFFTest {
     public void testExec_pos2_fail() {
         System.out.println("exec_pos2_fail");
         NodeValue v1 = NodeValue.makeFloat(0.0f);
-        NodeValue v2 = NodeValue.makeFloat(0.0f);
+        NodeValue v2 = NodeValue.makeFloat(1.0f);
         NodeValue v3 = NodeValue.makeString("10.0");
-        NodeValue v4 = NodeValue.makeFloat(10.0f);
+        NodeValue v4 = NodeValue.makeFloat(11.0f);
         ConvertLatLonBoxFF instance = new ConvertLatLonBoxFF();
-        NodeValue expResult = NodeValue.makeNode("<http://www.opengis.net/def/crs/EPSG/0/4326> POLYGON(0.0 0.0, 0.0 10.0, 10.0 10.0, 10.0 0.0, 0.0 0.0)", WKTDatatype.INSTANCE);
+        NodeValue expResult = NodeValue.makeNode("<http://www.opengis.net/def/crs/EPSG/0/4326> POLYGON((0.0 1.0, 10.0 1.0, 10.0 11.0, 0.0 11.0, 0.0 1.0))", WKTDatatype.INSTANCE);
         NodeValue result = instance.exec(v1, v2, v3, v4);
 
         //System.out.println("Exp: " + expResult);
@@ -139,11 +150,44 @@ public class ConvertLatLonBoxFFTest {
         NodeValue v3 = NodeValue.makeFloat(10.0f);
         NodeValue v4 = NodeValue.makeString("10.0");
         ConvertLatLonBoxFF instance = new ConvertLatLonBoxFF();
-        NodeValue expResult = NodeValue.makeNode("<http://www.opengis.net/def/crs/EPSG/0/4326> POLYGON(0.0 0.0, 0.0 10.0, 10.0 10.0, 10.0 0.0, 0.0 0.0)", WKTDatatype.INSTANCE);
+        NodeValue expResult = NodeValue.makeNode("<http://www.opengis.net/def/crs/EPSG/0/4326> POLYGON((0.0 1.0, 10.0 1.0, 10.0 11.0, 0.0 11.0, 0.0 1.0))", WKTDatatype.INSTANCE);
         NodeValue result = instance.exec(v1, v2, v3, v4);
 
         //System.out.println("Exp: " + expResult);
         //System.out.println("Res: " + result);
         //assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of exec method, of class ConvertLatLonBoxFF.
+     */
+    @Test
+    public void testExec_query() {
+        System.out.println("exec_query");
+
+        Dataset dataset = SpatialIndexTestData.createTestDataset();
+
+        String query = "PREFIX spatialF: <http://jena.apache.org/function/spatial#>\n"
+                + "\n"
+                + "SELECT ?result\n"
+                + "WHERE{\n"
+                + "    BIND( spatialF:convertLatLonBox(0.0, 1.0, 10.0, 11.0) AS ?result) \n"
+                + "}ORDER by ?result";
+
+        List<Literal> results = new ArrayList<>();
+        try (QueryExecution qe = QueryExecutionFactory.create(query, dataset)) {
+            ResultSet rs = qe.execSelect();
+            while (rs.hasNext()) {
+                QuerySolution qs = rs.nextSolution();
+                Literal result = qs.getLiteral("result");
+                results.add(result);
+            }
+        }
+
+        List<Literal> expResults = Arrays.asList(ResourceFactory.createTypedLiteral("<http://www.opengis.net/def/crs/EPSG/0/4326> POLYGON((0.0 1.0, 10.0 1.0, 10.0 11.0, 0.0 11.0, 0.0 1.0))", WKTDatatype.INSTANCE));
+
+        //System.out.println("Exp: " + expResults);
+        //System.out.println("Res: " + results);
+        assertEquals(expResults, results);
     }
 }
