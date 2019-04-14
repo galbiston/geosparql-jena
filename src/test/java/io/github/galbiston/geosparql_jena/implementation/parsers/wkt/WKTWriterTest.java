1c4369ec2d7cf8c19fc98cf8d3a9b3f39328a259
Greg Albiston <4662258+galbiston@users.noreply.github.com>
2019-04-14 15:00:53 +0100

    - WKTWriterTest: expanded writing tests including empty geometries.

--- src/test/java/io/github/galbiston/geosparql_jena/implementation/parsers/wkt/WKTWriterTest.java
+++ src/test/java/io/github/galbiston/geosparql_jena/implementation/parsers/wkt/WKTWriterTest.java
@@ -64,11 +64,29 @@ public class WKTWriterTest {
     private static final GeometryFactory GEOMETRY_FACTORY = CustomGeometryFactory.theInstance();
 
     /**
-     * Test of write method, of class WKTReader.
+     * Test of write method, of class WKTWriter.
      */
     @Test
     public void testWritePoint() {
         System.out.println("writePoint");
+        Geometry geometry = GEOMETRY_FACTORY.createPoint(new CustomCoordinateSequence(CoordinateSequenceDimensions.XY, "11.0 12.1"));
+
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(2, 2, 0));
+
+        String expResult = "<" + SRS_URI.WGS84_CRS + "> POINT (11 12.1)";
+        String result = WKTWriter.write(geometryWrapper);
+
+        //System.out.println("Expected: " + expResult);
+        //System.out.println("Result: " + result);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of write method, of class WKTWriter.
+     */
+    @Test
+    public void testWritePoint2() {
+        System.out.println("writePoint2");
         Geometry geometry = GEOMETRY_FACTORY.createPoint(new CustomCoordinateSequence(CoordinateSequenceDimensions.XYZM, "11.0 12.1 8.0 5.0"));
 
         GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 0));
@@ -82,14 +100,32 @@ public class WKTWriterTest {
     }
 
     /**
-     * Test of write method, of class WKTReader.
+     * Test of write method, of class WKTWriter.
      */
     @Test
     public void testWriteLineString() {
         System.out.println("writeLineString");
+        Geometry geometry = GEOMETRY_FACTORY.createLineString(new CustomCoordinateSequence(CoordinateSequenceDimensions.XY, "11.0 12.1, 3 4"));
+
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(2, 2, 1));
+
+        String expResult = "<" + SRS_URI.WGS84_CRS + "> LINESTRING (11 12.1, 3 4)";
+        String result = WKTWriter.write(geometryWrapper);
+
+        //System.out.println("Expected: " + expResult);
+        //System.out.println("Result: " + result);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of write method, of class WKTWriter.
+     */
+    @Test
+    public void testWriteLineString2() {
+        System.out.println("writeLineString2");
         Geometry geometry = GEOMETRY_FACTORY.createLineString(new CustomCoordinateSequence(CoordinateSequenceDimensions.XYZM, "11.0 12.1 8.0 5.0, 3 4 6 2"));
 
-        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 0));
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 1));
 
         String expResult = "<" + SRS_URI.WGS84_CRS + "> LINESTRING ZM(11 12.1 8 5, 3 4 6 2)";
         String result = WKTWriter.write(geometryWrapper);
@@ -100,14 +136,32 @@ public class WKTWriterTest {
     }
 
     /**
-     * Test of write method, of class WKTReader.
+     * Test of write method, of class WKTWriter.
      */
     @Test
     public void testWritePolygon() {
         System.out.println("writePolygon");
+        Geometry geometry = GEOMETRY_FACTORY.createPolygon(new CustomCoordinateSequence(CoordinateSequenceDimensions.XY, "30 10, 40 40, 20 40, 10 20, 30 10"));
+
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(2, 2, 2));
+
+        String expResult = "<" + SRS_URI.WGS84_CRS + "> POLYGON ((30 10, 40 40, 20 40, 10 20, 30 10))";
+        String result = WKTWriter.write(geometryWrapper);
+
+        //System.out.println("Expected: " + expResult);
+        //System.out.println("Result: " + result);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of write method, of class WKTWriter.
+     */
+    @Test
+    public void testWritePolygon2() {
+        System.out.println("writePolygon2");
         Geometry geometry = GEOMETRY_FACTORY.createPolygon(new CustomCoordinateSequence(CoordinateSequenceDimensions.XYZM, "30 10 0 1, 40 40 0 1, 20 40 0 1, 10 20 0 1, 30 10 0 1"));
 
-        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 0));
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 2));
 
         String expResult = "<" + SRS_URI.WGS84_CRS + "> POLYGON ZM((30 10 0 1, 40 40 0 1, 20 40 0 1, 10 20 0 1, 30 10 0 1))";
         String result = WKTWriter.write(geometryWrapper);
@@ -118,16 +172,16 @@ public class WKTWriterTest {
     }
 
     /**
-     * Test of write method, of class WKTReader.
+     * Test of write method, of class WKTWriter.
      */
     @Test
-    public void testWritePolygon2() {
-        System.out.println("writePolygon2");
+    public void testWritePolygon3() {
+        System.out.println("writePolygon3");
         LinearRing shell = GEOMETRY_FACTORY.createLinearRing(new CustomCoordinateSequence(CoordinateSequenceDimensions.XYZM, "30 10 0 1, 40 40 0 1, 20 40 0 1, 10 20 0 1, 30 10 0 1"));
         LinearRing[] holes = new LinearRing[]{GEOMETRY_FACTORY.createLinearRing(new CustomCoordinateSequence(CoordinateSequenceDimensions.XYZM, "20 30 0 1, 35 35 0 1, 30 20 0 1, 20 30 0 1"))};
         Geometry geometry = GEOMETRY_FACTORY.createPolygon(shell, holes);
 
-        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 0));
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 2));
 
         String expResult = "<" + SRS_URI.WGS84_CRS + "> POLYGON ZM((30 10 0 1, 40 40 0 1, 20 40 0 1, 10 20 0 1, 30 10 0 1), (20 30 0 1, 35 35 0 1, 30 20 0 1, 20 30 0 1))";
         String result = WKTWriter.write(geometryWrapper);
@@ -138,7 +192,7 @@ public class WKTWriterTest {
     }
 
     /**
-     * Test of write method, of class WKTReader.
+     * Test of write method, of class WKTWriter.
      */
     @Test
     public void testWriteMultiPoint() {
@@ -156,7 +210,7 @@ public class WKTWriterTest {
     }
 
     /**
-     * Test of write method, of class WKTReader.
+     * Test of write method, of class WKTWriter.
      */
     @Test
     public void testWriteMultiLineString() {
@@ -167,7 +221,7 @@ public class WKTWriterTest {
         lineStrings[1] = GEOMETRY_FACTORY.createLineString(new CustomCoordinateSequence(CoordinateSequenceDimensions.XYZM, "40 40 0 1, 30 30 0 1, 40 20 0 1, 30 10 0 1"));
         Geometry geometry = GEOMETRY_FACTORY.createMultiLineString(lineStrings);
 
-        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 0));
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 1));
 
         String expResult = "<" + SRS_URI.WGS84_CRS + "> MULTILINESTRING ZM((10 10 0 1, 20 20 0 1, 10 40 0 1), (40 40 0 1, 30 30 0 1, 40 20 0 1, 30 10 0 1))";
         String result = WKTWriter.write(geometryWrapper);
@@ -178,11 +232,33 @@ public class WKTWriterTest {
     }
 
     /**
-     * Test of write method, of class WKTReader.
+     * Test of extract method, of class WKTWriter.
      */
     @Test
     public void testWriteMultiPolygon() {
         System.out.println("writeMultiPolygon");
+
+        Polygon[] polygons = new Polygon[2];
+        polygons[0] = GEOMETRY_FACTORY.createPolygon(new CustomCoordinateSequence(CoordinateSequenceDimensions.XY, "40 40, 20 45, 45 30, 40 40"));
+        polygons[1] = GEOMETRY_FACTORY.createPolygon(new CustomCoordinateSequence(CoordinateSequenceDimensions.XY, "20 35, 10 30, 10 10, 30 5, 45 20, 20 35"));
+        Geometry geometry = GEOMETRY_FACTORY.createMultiPolygon(polygons);
+
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(2, 2, 2));
+
+        String expResult = "<" + SRS_URI.WGS84_CRS + "> MULTIPOLYGON (((40 40, 20 45, 45 30, 40 40)), ((20 35, 10 30, 10 10, 30 5, 45 20, 20 35)))";
+        String result = WKTWriter.write(geometryWrapper);
+
+        //System.out.println("Expected: " + expResult);
+        //System.out.println("Result: " + result);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of write method, of class WKTWriter.
+     */
+    @Test
+    public void testWriteMultiPolygon2() {
+        System.out.println("writeMultiPolygon2");
 
         Polygon[] polygons = new Polygon[2];
         polygons[0] = GEOMETRY_FACTORY.createPolygon(new CustomCoordinateSequence(CoordinateSequenceDimensions.XYZM, "40 40 0 1, 20 45 0 1, 45 30 0 1, 40 40 0 1"));
@@ -191,7 +267,7 @@ public class WKTWriterTest {
         polygons[1] = GEOMETRY_FACTORY.createPolygon(shell, holes);
         Geometry geometry = GEOMETRY_FACTORY.createMultiPolygon(polygons);
 
-        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 0));
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 2));
 
         String expResult = "<" + SRS_URI.WGS84_CRS + "> MULTIPOLYGON ZM(((40 40 0 1, 20 45 0 1, 45 30 0 1, 40 40 0 1)), ((20 35 0 1, 10 30 0 1, 10 10 0 1, 30 5 0 1, 45 20 0 1, 20 35 0 1), (30 20 0 1, 20 15 0 1, 20 25 0 1, 30 20 0 1)))";
         String result = WKTWriter.write(geometryWrapper);
@@ -202,7 +278,7 @@ public class WKTWriterTest {
     }
 
     /**
-     * Test of write method, of class WKTReader.
+     * Test of write method, of class WKTWriter.
      */
     @Test
     public void testWriteGeometryCollection() {
@@ -213,10 +289,126 @@ public class WKTWriterTest {
         geometries[1] = GEOMETRY_FACTORY.createLineString(new CustomCoordinateSequence(CoordinateSequenceDimensions.XYZM, "4 6 0 1,7 10 0 1"));
         Geometry geometry = GEOMETRY_FACTORY.createGeometryCollection(geometries);
 
-        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 0));
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.WGS84_CRS, WKTDatatype.URI, new DimensionInfo(4, 3, 1));
 
         String expResult = "<" + SRS_URI.WGS84_CRS + "> GEOMETRYCOLLECTION ZM(POINT ZM(4 6 0 1), LINESTRING ZM(4 6 0 1, 7 10 0 1))";
         String result = WKTWriter.write(geometryWrapper);
+
+        //System.out.println("Expected: " + expResult);
+        //System.out.println("Result: " + result);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of writePointEmpty method, of class WKTWriter.
+     */
+    @Test
+    public void testWritePointEmpty() {
+        System.out.println("writePointEmpty");
+
+        Geometry geometry = GEOMETRY_FACTORY.createPoint();
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.DEFAULT_WKT_CRS84, WKTDatatype.URI, new DimensionInfo(2, 2, 0));
+        String result = WKTWriter.write(geometryWrapper);
+        String expResult = "POINT EMPTY";
+
+        //System.out.println("Expected: " + expResult);
+        //System.out.println("Result: " + result);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of writeLineStringEmpty method, of class WKTWriter.
+     */
+    @Test
+    public void testWriteLineStringEmpty() {
+        System.out.println("writeLineEmpty");
+        Geometry geometry = GEOMETRY_FACTORY.createLineString();
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.DEFAULT_WKT_CRS84, WKTDatatype.URI, new DimensionInfo(2, 2, 1));
+        String result = WKTWriter.write(geometryWrapper);
+        String expResult = "LINESTRING EMPTY";
+
+        //System.out.println("Expected: " + expResult);
+        //System.out.println("Result: " + result);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of writePolygonEmpty method, of class WKTWriter.
+     */
+    @Test
+    public void testWritePolygonEmpty() {
+        System.out.println("writePolygonEmpty");
+        Geometry geometry = GEOMETRY_FACTORY.createPolygon();
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.DEFAULT_WKT_CRS84, WKTDatatype.URI, new DimensionInfo(2, 2, 2));
+        String result = WKTWriter.write(geometryWrapper);
+        String expResult = "POLYGON EMPTY";
+
+        //System.out.println("Expected: " + expResult);
+        //System.out.println("Result: " + result);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of writeMultiPointEmpty method, of class WKTWriter.
+     */
+    @Test
+    public void testWriteMultiPointEmpty() {
+        System.out.println("writeMultiPointEmpty");
+        Geometry geometry = GEOMETRY_FACTORY.createMultiPoint();
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.DEFAULT_WKT_CRS84, WKTDatatype.URI, new DimensionInfo(2, 2, 0));
+        String result = WKTWriter.write(geometryWrapper);
+        String expResult = "MULTIPOINT EMPTY";
+
+        //System.out.println("Expected: " + expResult);
+        //System.out.println("Result: " + result);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of writeMultiLineString method, of class WKTWriter.
+     */
+    @Test
+    public void testWriteMultiLineStringEmpty() {
+        System.out.println("writeMultiLineStringEmpty");
+
+        Geometry geometry = GEOMETRY_FACTORY.createMultiLineString();
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.DEFAULT_WKT_CRS84, WKTDatatype.URI, new DimensionInfo(2, 2, 1));
+        String result = WKTWriter.write(geometryWrapper);
+        String expResult = "MULTILINESTRING EMPTY";
+
+        //System.out.println("Expected: " + expResult);
+        //System.out.println("Result: " + result);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of writeMultiPolygonEmpty method, of class WKTWriter.
+     */
+    @Test
+    public void testWriteMultiPolygonEmpty() {
+        System.out.println("writeMultiPolygonEmpty");
+
+        Geometry geometry = GEOMETRY_FACTORY.createMultiPolygon();
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.DEFAULT_WKT_CRS84, WKTDatatype.URI, new DimensionInfo(2, 2, 2));
+        String result = WKTWriter.write(geometryWrapper);
+        String expResult = "MULTIPOLYGON EMPTY";
+
+        //System.out.println("Expected: " + expResult);
+        //System.out.println("Result: " + result);
+        assertEquals(expResult, result);
+    }
+
+    /**
+     * Test of writeGeometryCollectionEmpty method, of class WKTWriter.
+     */
+    @Test
+    public void testWriteGeometryCollectionEmpty() {
+        System.out.println("writeGeometryCollectionEmpty");
+
+        Geometry geometry = GEOMETRY_FACTORY.createGeometryCollection();
+        GeometryWrapper geometryWrapper = new GeometryWrapper(geometry, SRS_URI.DEFAULT_WKT_CRS84, WKTDatatype.URI, new DimensionInfo(2, 2, 2));
+        String result = WKTWriter.write(geometryWrapper);
+        String expResult = "GEOMETRYCOLLECTION EMPTY";
 
         //System.out.println("Expected: " + expResult);
         //System.out.println("Result: " + result);
